e660b26b4f4f93ffd128c9b35aa0dde637017eda
Django Freeman <kshohrux@mail.ru>
2020-07-19 21:30:05 +0500

    Serialization started

--- insurance/urls.py
+++ insurance/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 from insurance import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register('position', viewset=views.PositionsViewSet)
+router.register('profile', viewset=views.ProfileViewSet)
+
 
 urlpatterns = [
     path('test/', views.test_view),
+    path('api/', include(router.urls)),
 ]
--- insurance/views.py
+++ insurance/views.py
@@ -1,9 +1,11 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from insurance.serializers import *
+from rest_framework import viewsets
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -14,8 +16,38 @@ def test_view(request):
     return Response(response)
 
 
-
-
-
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class PositionsViewSet(viewsets.ModelViewSet):
+    #permission_classes = [IsAuthenticated, ]
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
+    # def create(self, request, *args, **kwargs):
+    #     print("Create")
+    #     return Response(self.request.data)
+    #
+    # def update(self, request, *args, **kwargs):
+    #     print("update")
+    #     return Response(self.request.data)
+
+    # def destroy(self, request, *args, **kwargs):
+    #     print("DELETE")
+    #     return Response(self.request.data)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
